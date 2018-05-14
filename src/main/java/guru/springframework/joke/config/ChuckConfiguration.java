a2ea28837635e89d77fb09ac81df7c38d5b442ed
Piyush2509 <piyush.jayaswal@gmail.com>
2018-05-14 15:46:22 -0400

    Spring XML configuration example

--- src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/joke/config/ChuckConfiguration.java
@@ -1,17 +1,14 @@
 package guru.springframework.joke.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 /**
  * Created by piyush.b.kumar on May 14, 2018.
  */
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 	
-	@Bean
+	//@Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
